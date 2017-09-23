da321f08a4cbc348f46c2f2085bdd669c35e6156
Rodion Mescheryakov <msrodion@gmail.com>
2017-09-23 17:57:25 +0300

    Task Model and Service are ready

--- src/app/models/task.ts
+++ src/app/models/task.ts
@@ -1,2 +1,16 @@
 export class Task {
+  id: number;
+  name: string;
+  creation_date: string;
+  due_date: string;
+  start_date: string;
+  is_completed: boolean;
+  is_archived: boolean;
+  estimated_effort: number;
+  actual_effort: number;
+  physical_progress: number;
+  obj_status: string;
+  description: string;
+  project_id: number;
+  tags: string[];
 }
--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,8 +1,39 @@
 import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+
+import { Observable } from 'rxjs/Observable';
+
+import { Task } from '../models/task';
 
 @Injectable()
 export class TaskService {
+  private tasksUrl = 'api/tasks';
 
-  constructor() { }
+  constructor(private http: Http) {}
 
+  getTasks(): Observable<Task[]> {
+    return this.http
+      .get(this.tasksUrl)
+      .map(response => response.json().data as Task[])
+      .catch(this._handleError);
+  }
+
+  getTask(id: number): Observable<Task> {
+    const url = `${this.tasksUrl}/${id}`;
+
+    return this.http
+      .get(url)
+      .map(response => response.json().data as Task)
+      .catch(this._handleError);
+  }
+
+  private _handleError(err: any) {
+    console.log('sever error:', err);
+
+    if (err instanceof Response) {
+      return Observable.throw(err.json().error || 'backend server error');
+    }
+
+    return Observable.throw(err || 'backend server error');
+  }
 }
