a5d4e43db7558a71371b2789ee257d7ce63e2065
Rodion Mescheryakov <msrodion@gmail.com>
2017-09-23 19:10:01 +0300

    TaskDetail component implemented. TaskDetail markup is in progress

--- src/app/task-detail/task-detail.component.ts
+++ src/app/task-detail/task-detail.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { Location } from '@angular/common';
+
+import 'rxjs/add/operator/switchMap';
+
+import { Task } from '../models/task';
+import { TaskService } from '../services/task.service';
 
 @Component({
   selector: 'app-task-detail',
@@ -6,10 +13,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./task-detail.component.scss']
 })
 export class TaskDetailComponent implements OnInit {
+  @Input() task: Task;
 
-  constructor() { }
+  constructor(
+    private taskService: TaskService,
+    private route: ActivatedRoute,
+    private location: Location
+  ) {}
 
-  ngOnInit() {
+  ngOnInit(): void {
+    this.route.paramMap
+      .switchMap((params: ParamMap) => this.taskService.getTask(+params.get('id')))
+      .subscribe(task => this.task = task);
   }
 
+  goBack(): void {
+    this.location.back();
+  }
+
+  save(): void {
+    this.taskService.update(this.task);
+  }
 }
