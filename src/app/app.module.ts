d38a741d19a27097b9d9a62316c5376cc67202ff
Rodion Mescheryakov <msrodion@gmail.com>
2017-09-23 19:08:45 +0300

    App module updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
 
+import { InMemoryDataService } from './services/in-memory-data.service';
 import { AppRoutingModule } from './app-routing.module';
+import { TaskService } from './services/task.service';
 import { AppComponent } from './app.component';
 import { TasksComponent } from './tasks/tasks.component';
 import { TaskDetailComponent } from './task-detail/task-detail.component';
-import { TaskService } from './services/task.service';
 
 @NgModule({
+  imports: [
+    BrowserModule,
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
+    AppRoutingModule
+  ],
+  providers: [TaskService],
   declarations: [
     AppComponent,
     TasksComponent,
     TaskDetailComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [TaskService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
