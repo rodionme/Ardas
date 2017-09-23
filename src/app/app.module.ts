1f893f3fe4b0d0fcecca74f1315507219ab7ca2a
Rodion Mescheryakov <msrodion@gmail.com>
2017-09-23 18:10:21 +0300

    Router module imported to App module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TasksComponent } from './tasks/tasks.component';
 import { TaskDetailComponent } from './task-detail/task-detail.component';
@@ -13,7 +14,8 @@ import { TaskService } from './services/task.service';
     TaskDetailComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [TaskService],
   bootstrap: [AppComponent]
