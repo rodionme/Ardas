a69d406487eaebb8dd79f521b5065fcce3964699
Rodion Mescheryakov <msrodion@gmail.com>
2017-09-23 19:08:21 +0300

    Tasks component implemented. Tasks markup is in progress

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 
+import { TaskService } from '../services/task.service';
+import { Task } from '../models/task';
+
 @Component({
   selector: 'app-tasks',
   templateUrl: './tasks.component.html',
   styleUrls: ['./tasks.component.scss']
 })
 export class TasksComponent implements OnInit {
+  tasks: Task[];
+  activeTasks: number;
+  totalTasks: number;
 
-  constructor() { }
+  constructor(private taskService: TaskService) {}
 
   ngOnInit() {
+    this.getActiveTasks();
   }
 
+  getTasks(): void {
+    this.taskService.getTasks().subscribe(tasks => this.tasks = tasks);
+  }
+
+  getActiveTasks(): void {
+    this.taskService.getTasks()
+      .subscribe(tasks => {
+        this.tasks = tasks.filter(task => task.obj_status === 'active');
+
+        this.activeTasks = this.tasks.length;
+        this.totalTasks = tasks.length;
+      });
+  }
 }
