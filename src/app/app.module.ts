1d77b7ccbabd56a2b1a1e04cd5e08a49cd999386
Rodion Mescheryakov <msrodion@gmail.com>
2017-09-23 10:55:17 +0300

    Structure components of the app added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TasksComponent } from './tasks/tasks.component';
+import { TaskDetailComponent } from './task-detail/task-detail.component';
+import { TaskService } from './services/task.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TasksComponent,
+    TaskDetailComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [TaskService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
